4c0fcdfece3a56b9675b77456c6d2aca16354d24
danichagas <chagasdaniel788@gmail.com>
2026-07-12 17:02:44 -0300

    docs(database): banco atualizado

--- gametracker_db.sql
+++ gametracker_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10/07/2026 às 04:04
+-- Tempo de geração: 12/07/2026 às 22:01
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -35,15 +35,38 @@ CREATE TABLE `jogos` (
   `review` text DEFAULT NULL,
   `data_cadastro` timestamp NOT NULL DEFAULT current_timestamp(),
   `genero` varchar(50) DEFAULT NULL,
-  `ano_lancamento` varchar(10) DEFAULT NULL
+  `ano_lancamento` varchar(10) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `jogos`
 --
 
-INSERT INTO `jogos` (`id`, `nome`, `status_jogo`, `nota`, `review`, `data_cadastro`, `genero`, `ano_lancamento`) VALUES
-(4, 'God of War', 'Platinado', 10, 'Zerei e peguei todos os troféus!', '2026-07-09 20:34:48', 'Action', '2005');
+INSERT INTO `jogos` (`id`, `nome`, `status_jogo`, `nota`, `review`, `data_cadastro`, `genero`, `ano_lancamento`, `usuario_id`) VALUES
+(5, 'Cyberpunk 2077', 'Jogando', 8, 'Cheio de bugs, mas a historia é boa', '2026-07-12 19:25:55', 'Shooter', '2020', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `data_cadastro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `data_cadastro`) VALUES
+(1, 'Daniel', 'chagas@gmail.com', '$2y$10$PjulH03Zoz9CMA6HgRQaQupdBr74YIq2fSoxdm5fCu1SwJZuhzOl2', '2026-07-12 19:14:03'),
+(2, 'Elder', 'aragao@gmail.com', '$2y$10$Ui5hUUzgWIU9fSaZDjdG1uG4teIHD2VDUcH3yfT0.qmxJ8EsBvUB.', '2026-07-12 19:14:58');
 
 --
 -- Índices para tabelas despejadas
@@ -53,7 +76,15 @@ INSERT INTO `jogos` (`id`, `nome`, `status_jogo`, `nota`, `review`, `data_cadast
 -- Índices de tabela `jogos`
 --
 ALTER TABLE `jogos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_usuario` (`usuario_id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -63,7 +94,23 @@ ALTER TABLE `jogos`
 -- AUTO_INCREMENT de tabela `jogos`
 --
 ALTER TABLE `jogos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `jogos`
+--
+ALTER TABLE `jogos`
+  ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
